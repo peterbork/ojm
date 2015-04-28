7ddfbd378647fcfd3ef3eacc0fbfb627d5aba72e
nickosv <niickj@live.dk>
2015-04-28 09:48:29 +0200

    Storage DB conn

--- ojm/Controllers/DatabaseFacade.cs
+++ ojm/Controllers/DatabaseFacade.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ojm.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
 
 namespace ojm.Controllers {
     static class DatabaseFacade {
+        static string ConnectionString = "Server=ealdb1.eal.local;" + "Database=ejl26_db;" + "User Id=ejl26_usr;" + "Password=Baz1nga26";
 
         // CUSTOMER METHODS
         public static bool IsCustomerExsisting(string cvr) {
@@ -21,12 +25,38 @@ namespace ojm.Controllers {
         // STORAGE METHODS
         public static void UpdateStorageItem(Product product)
         {
-            
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            try 
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("[StoredProcedure Name]", conn); 
+                cmd.CommandType = CommandType.StoredProcedure; 
+                cmd.Parameters.Add(new SqlParameter("ID", product.ID));
+                cmd.Parameters.Add(new SqlParameter("Name", product.Name));
+                cmd.Parameters.Add(new SqlParameter("InStock", product.InStock));
+                cmd.ExecuteNonQuery(); 
+            } 
+            catch (SqlException e) 
+            { 
+                MessageBox.Show(e.Message); 
+            } 
         }
 
         public static void RegisterDelivery(Delivery delivery)
         {
-            
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("[StoredProcedure Name]", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("ID", delivery.ID));
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show(e.Message);
+            } 
         }
     }
 }
