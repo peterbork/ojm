c142e990cb2050f6b3a5dce0ff4bd2bb19212201
Futtelutdut <fred3760@edu.eal.dk>
2015-05-07 10:58:26 +0200

    Fucked Up ProductOrderView

---
+++ ojm/Views/ProductOrderView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ojm.Views
+{
+    /// <summary>
+    /// Interaction logic for ProductOrderView.xaml
+    /// </summary>
+    public partial class ProductOrderView : Window
+    {
+        public ProductOrderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
