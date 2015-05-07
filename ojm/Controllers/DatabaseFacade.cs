fcfe838f74cb2440929c530657e8d6c72937defb
nicoeg <nicomanden@gmail.com>
2015-05-07 12:42:13 +0200

    Added regions again

--- ojm/Controllers/DatabaseFacade.cs
+++ ojm/Controllers/DatabaseFacade.cs
@@ -12,6 +12,8 @@ namespace ojm.Controllers {
     static class DatabaseFacade {
         static string ConnectionString = "Server=ealdb1.eal.local;" + "Database=ejl26_db;" + "User Id=ejl26_usr;" + "Password=Baz1nga26";
 
+
+        #region Customer
         // CUSTOMER METHODS
         public static Customer GetCustomerFromCVR(string cvr) {
             Customer _customer = new Customer();
@@ -138,7 +140,8 @@ namespace ojm.Controllers {
             return _customerList;
         }
 
-
+        #endregion
+        #region Materials
         // STORAGE METHODS
         public static List<Material> GetMaterials()
         {
@@ -328,7 +331,8 @@ namespace ojm.Controllers {
             }
         }
 
-
+        #endregion
+        #region ProductOrder
 
         internal static void AddProductOrder(ProductOrder productorder)
         {
@@ -362,5 +366,7 @@ namespace ojm.Controllers {
                 conn.Dispose();
             }
         }
+
+        #endregion
     }
 }
