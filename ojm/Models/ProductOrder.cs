bdb953545ac6dfd4c4d9a665f6699554f650add0
Futtelutdut <fred3760@edu.eal.dk>
2015-05-07 09:51:29 +0200

    ProductOrder cleanout

--- ojm/Models/ProductOrder.cs
+++ ojm/Models/ProductOrder.cs
@@ -8,6 +8,13 @@ namespace ojm.Models
 {
     class ProductOrder
     {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Material> Materials { get; set; }
+        public Customer Customer { get; set; }
+
         public ProductOrder(int id, string name, string description, Customer customer)
         {
             ID = id;
@@ -15,10 +22,12 @@ namespace ojm.Models
             Description = description;
             Customer = customer;
         }
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Material> Materials { get; set; }
-        public Customer Customer { get; set; }
+        public ProductOrder(string name, string description, Customer customer, List<Material> materials)
+        {
+            Name = name;
+            Description = description;
+            Customer = customer;
+            Materials = materials;
+        }
     }
 }
